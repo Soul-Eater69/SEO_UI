ba8ca44e08a3afc4ce42fa510219a9d7dd6173aa
Soul-Eater69 <51698530+Soul-Eater69@users.noreply.github.com>
2023-04-11 14:40:27 +0530

    updated grade calculation logic

--- src/components/lib.js
+++ src/components/lib.js
@@ -7,63 +7,81 @@ export const gradeCalculator = ({
 }) => {
   let grade = "";
   let recommendation = "";
+  let site_priority_score = 0;
+  let total_priority_score = 0;
 
   const total_recommendations = recommendations.length;
   let total_data_elements = 0;
 
-  Object.keys(seoData).forEach((key) =>
-    seoData[key].required && seoData[key].required == 1
-      ? (total_data_elements += 1)
-      : null
-  );
-  Object.keys(usabilityData).forEach((key) =>
-    usabilityData[key].required && usabilityData[key].required == 1
-      ? (total_data_elements += 1)
-      : null
-  );
-  Object.keys(performanceData).forEach((key) =>
-    performanceData[key].required && performanceData[key].required == 1
-      ? (total_data_elements += 1)
-      : null
-  );
-  Object.keys(socialData).forEach((key) =>
-    socialData[key].required && socialData[key].required == 1
-      ? (total_data_elements += 1)
-      : null
+  site_priority_score = recommendations.reduce((acc, curr) => {
+    acc += curr.priority || 0;
+    return acc;
+  }, 0);
+
+  Object.keys(seoData).forEach((key) => {
+    if (seoData[key].required && seoData[key].required == 1) {
+      total_priority_score += seoData[key].priority || 0;
+    }
+  });
+  Object.keys(usabilityData).forEach((key) => {
+    if (usabilityData[key].required && usabilityData[key].required == 1) {
+      total_priority_score += usabilityData[key].priority || 0;
+    }
+  });
+  Object.keys(performanceData).forEach((key) => {
+    if (performanceData[key].required && performanceData[key].required == 1) {
+      total_priority_score += performanceData[key].priority || 0;
+    }
+  });
+  Object.keys(socialData).forEach((key) => {
+    if (socialData[key].required && socialData[key].required == 1) {
+      total_priority_score += socialData[key].priority || 0;
+    }
+  });
+
+  const priorityGradePercentage = Math.floor(
+    100 -
+      (parseFloat(site_priority_score) / parseFloat(total_priority_score)) * 100
   );
 
   const gradePercentage =
     100 -
     (parseFloat(total_recommendations) / parseFloat(total_data_elements)) * 100;
 
-  if (gradePercentage >= 95) {
+  console.log(
+    site_priority_score,
+    total_priority_score,
+    priorityGradePercentage,
+    gradePercentage
+  );
+  if (priorityGradePercentage >= 95) {
     grade = "A+";
     recommendation = "Your page is excellent!";
-  } else if (gradePercentage >= 90) {
+  } else if (priorityGradePercentage >= 90) {
     grade = "A";
     recommendation = "Your page is very good!";
-  } else if (gradePercentage >= 85) {
+  } else if (priorityGradePercentage >= 85) {
     grade = "A-";
     recommendation = "Your page is good!";
-  } else if (gradePercentage >= 80) {
+  } else if (priorityGradePercentage >= 80) {
     grade = "B+";
     recommendation = "Your page is above average!";
-  } else if (gradePercentage >= 75) {
+  } else if (priorityGradePercentage >= 75) {
     grade = "B";
     recommendation = "Your page is average.";
-  } else if (gradePercentage >= 70) {
+  } else if (priorityGradePercentage >= 70) {
     grade = "B-";
     recommendation = "Your page is slightly below average.";
-  } else if (gradePercentage >= 65) {
+  } else if (priorityGradePercentage >= 65) {
     grade = "C+";
     recommendation = "Your page needs improvement.";
-  } else if (gradePercentage >= 60) {
+  } else if (priorityGradePercentage >= 60) {
     grade = "C";
     recommendation = "Your page needs significant improvement.";
-  } else if (gradePercentage >= 55) {
+  } else if (priorityGradePercentage >= 50) {
     grade = "C-";
     recommendation = "Your page is poor.";
-  } else if (gradePercentage >= 50) {
+  } else if (priorityGradePercentage >= 40) {
     grade = "D";
     recommendation = "Your page is very poor.";
   } else {
@@ -71,5 +89,5 @@ export const gradeCalculator = ({
     recommendation = "Your page needs serious attention.";
   }
 
-  return { recommendation, grade, gradePercentage };
+  return { recommendation, grade, priorityGradePercentage };
 };
